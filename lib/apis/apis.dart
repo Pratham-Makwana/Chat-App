c0d179a395450a2c2558c1a1f576d6baa6724b6a
Pratham-Makwana <makwanapratham13@gmail.com>
2024-08-25 18:27:26 +0530

    Modify The Profile Query In Firerbase

--- lib/apis/apis.dart
+++ lib/apis/apis.dart
@@ -12,9 +12,23 @@ class APIs {
   /// to return current user
   static User get user => auth.currentUser!;
 
+  /// for storing self info
+  static late ChatUser me;
+
   /// for checking user is exits or not ?
   static Future<bool> userExists() async {
     return (await firestore.collection('user').doc(user.uid).get()).exists;
+  }
+
+  /// for getting current user info
+  static Future<void> getSelfInfo() async {
+    await firestore.collection('user').doc(user.uid).get().then((user) async {
+      if (user.exists) {
+        me = ChatUser.fromJson(user.data()!);
+      } else {
+        await createUser().then((value) => getSelfInfo());
+      }
+    });
   }
 
   /// for creating new user
@@ -32,10 +46,23 @@ class APIs {
         pushToken: '',
         email: user.email.toString());
 
-
     await firestore.collection('user').doc(user.uid).set(chatUser.toJson());
   }
 
+  /// for getting all users from firebase database
+  static Stream<QuerySnapshot<Map<String, dynamic>>> getAllUsers() {
+    return APIs.firestore
+        .collection("user")
+        .where('id', isNotEqualTo: user.uid)
+        .snapshots();
+  }
 
 
+  /// for updating user information
+  static Future<void> updateUserInfo() async {
+    await firestore
+        .collection('user')
+        .doc(user.uid)
+        .update({'name': me.name, 'about': me.about});
+  }
 }
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,5 +1,7 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:chatapp/apis/apis.dart';
+import 'package:chatapp/helper/dialogs.dart';
+import 'package:chatapp/screens/auth/login_screen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -17,128 +19,190 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     /// initializing media query for getting device screen size
     mq = MediaQuery.of(context).size;
-    return Scaffold(
-      /// AppBar
-      appBar: AppBar(
-        title: const Text(
-          "Chat App",
-        ),
-      ),
-
-      /// floating action button to add user
-      floatingActionButton: Padding(
-        padding: const EdgeInsets.only(bottom: 10),
-        child: FloatingActionButton.extended(
-          backgroundColor: Colors.redAccent,
-          onPressed: () async {
-            await APIs.auth.signOut();
-            await GoogleSignIn().signOut();
-          },
-          label: const Text(
-            "Logout",
-            style: TextStyle(fontSize: 18, color: Colors.white),
-          ),
-          icon: const Icon(
-            Icons.login_outlined,
-            color: Colors.white,
+    return GestureDetector(
+      /// for hiding keyboard anywhere you click on screen
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        /// AppBar
+        appBar: AppBar(
+          title: const Text(
+            "Chat App",
           ),
         ),
-      ),
 
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: mq.height * .03),
-          child: Column(
-            children: [
-              /// For adding some space
-              SizedBox(
-                width: mq.width,
-                height: mq.height * .05,
-              ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(mq.height * .1),
-                child: CachedNetworkImage(
-                  height: mq.height * .2,
-                  width: mq.height * .2,
-                  fit: BoxFit.fill,
-                  imageUrl : widget.user.image,
-                  //imageUrl: 'https://imgs.search.brave.com/WFiTFcSi2_uNMo5T034sJyD6x7tVkY1nBmdnTRLknp4/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTQw/NzA4NDQxOS9waG90/by9wb3J0cmFpdC1v/Zi1oYXBweS1tYXR1/cmUtd29tZW4uanBn/P3M9NjEyeDYxMiZ3/PTAmaz0yMCZjPVRW/ak5iSHlyU1ZKN1hj/VFpONi1sSG1JdEhJ/RjB2VnpjSXZPWXlY/QVBwOGs9',
-                  // placeholder: (context, url) => CircularProgressIndicator(),
-                  errorWidget: (context, url, error) =>
-                      const CircleAvatar(child: Icon(CupertinoIcons.person)),
-                ),
-              ),
-        
-              /// For adding some space
-              SizedBox(
-                height: mq.height * .03,
-              ),
+        /// floating action button to add user
+        floatingActionButton: Padding(
+          padding: const EdgeInsets.only(bottom: 10),
+          child: FloatingActionButton.extended(
+            backgroundColor: Colors.redAccent,
+            onPressed: () async {
+              /// for showing progress dialog box
+              Dialogs.showProgressBar(context);
 
-              /// user email label
-              Text(
-                widget.user.email,
-                style: const TextStyle(fontSize: 16, color: Colors.black87
+              /// sign out from app
+              await APIs.auth.signOut().then((value) async {
+                await GoogleSignIn().signOut().then((value) {
+                  /// for hiding process dialog
+                  Navigator.pop(context);
 
-                ),
-              ),
-        
-              /// For adding some space
-              SizedBox(
-                height: mq.height * .05,
-              ),
+                  /// for moving to home screen
+                  Navigator.pop(context);
 
-              /// name input filed
-              TextFormField(
-                initialValue: widget.user.name,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    label: const Text("Name"),
-                    prefixIcon: const Icon(
-                      Icons.person,
-                      color: Colors.black,
-                    )),
-              ),
-        
-              /// For adding some space
-              SizedBox(
-                height: mq.height * .02,
-              ),
+                  /// replacing home screen with login screen
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (_) => const LoginScreen()));
+                });
+              });
+            },
+            label: const Text(
+              "Logout",
+              style: TextStyle(fontSize: 18, color: Colors.white),
+            ),
+            icon: const Icon(
+              Icons.login_outlined,
+              color: Colors.white,
+            ),
+          ),
+        ),
 
-              /// about input field
-              TextFormField(
-                initialValue: widget.user.about,
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(15),
-                    ),
-                    label: const Text("About"),
-                    prefixIcon: const Icon(
-                      Icons.info_outline,
-                      color: Colors.black,
-                    )),
-              ),
-        
-              /// For adding some space
-              SizedBox(
-                height: mq.height * .02,
-              ),
+        body: Form(
+          key: _formKey,
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: mq.height * .03),
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  /// For adding some space
+                  SizedBox(
+                    width: mq.width,
+                    height: mq.height * .05,
+                  ),
+                  Stack(
+                    children: [
+                      /// profile picture
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(mq.height * .1),
+                        child: CachedNetworkImage(
+                          height: mq.height * .2,
+                          width: mq.height * .2,
+                          fit: BoxFit.fill,
+                          imageUrl: widget.user.image,
+                          errorWidget: (context, url, error) =>
+                              const CircleAvatar(
+                                  child: Icon(CupertinoIcons.person)),
+                        ),
+                      ),
 
-              /// update profile button
-              ElevatedButton.icon(
-                style:ElevatedButton.styleFrom(
-                  minimumSize: Size(double.infinity, mq.height * .06),
-                  backgroundColor: Colors.deepPurple.shade500
-                ),
-                  onPressed: () {},
-                  icon: const Icon(Icons.edit,color: Colors.white,size: 24,),
-                  label: const Text('Update',style: TextStyle(color: Colors.white,fontSize: 16),))
-            ],
+                      /// edit image button
+                      Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: MaterialButton(
+                          elevation: 1,
+                          onPressed: () {},
+                          shape: const CircleBorder(),
+                          color: Colors.white,
+                          child: const Icon(
+                            Icons.edit,
+                            color: Colors.deepPurple,
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+
+                  /// For adding some space
+                  SizedBox(
+                    height: mq.height * .03,
+                  ),
+
+                  /// user email label
+                  Text(
+                    widget.user.email,
+                    style: const TextStyle(fontSize: 16, color: Colors.black87),
+                  ),
+
+                  /// For adding some space
+                  SizedBox(
+                    height: mq.height * .05,
+                  ),
+
+                  /// name input filed
+                  TextFormField(
+                    initialValue: widget.user.name,
+                    onSaved: (val) => APIs.me.name = val ?? '',
+                    validator: (val) =>
+                        val != null && val.isNotEmpty ? null : 'Required Field',
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        label: const Text("Name"),
+                        prefixIcon: const Icon(
+                          Icons.person,
+                          color: Colors.black,
+                        )),
+                  ),
+
+                  /// For adding some space
+                  SizedBox(
+                    height: mq.height * .02,
+                  ),
+
+                  /// about input field
+                  TextFormField(
+                    initialValue: widget.user.about,
+                    onSaved: (val) => APIs.me.about = val ?? '',
+                    validator: (val) =>
+                        val != null && val.isNotEmpty ? null : 'Required Field',
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        label: const Text("About"),
+                        prefixIcon: const Icon(
+                          Icons.info_outline,
+                          color: Colors.black,
+                        )),
+                  ),
+
+                  /// For adding some space
+                  SizedBox(
+                    height: mq.height * .02,
+                  ),
+
+                  /// update profile button
+                  ElevatedButton.icon(
+                      style: ElevatedButton.styleFrom(
+                          minimumSize: Size(double.infinity, mq.height * .06),
+                          backgroundColor: Colors.deepPurple.shade500),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          _formKey.currentState!.save();
+                          APIs.updateUserInfo().then((value) {
+                            Dialogs.showSnackBar(
+                                context, "Profile Update Successfully");
+                          });
+                        }
+                      },
+                      icon: const Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                        size: 24,
+                      ),
+                      label: const Text(
+                        'Update',
+                        style: TextStyle(color: Colors.white, fontSize: 16),
+                      ))
+                ],
+              ),
+            ),
           ),
         ),
       ),
