fa9d74311ac33304bbb78ec85d74bfaa916a67e8
Pratham-Makwana <makwanapratham13@gmail.com>
2024-08-27 18:12:10 +0530

    Message Model To Store & Fetch Data

---
+++ lib/model/message.dart
@@ -0,0 +1,42 @@
+class Message {
+  Message({
+    required this.toId,
+    required this.msg,
+    required this.read,
+    required this.type,
+    required this.fromId,
+    required this.sent,
+  });
+
+  late String toId;
+  late String msg;
+  late String read;
+  late String fromId;
+  late String sent;
+  late Type type;
+
+  Message.fromJson(Map<String, dynamic> json) {
+    toId = json['toId'].toString();
+    msg = json['msg'].toString();
+    read = json['read'].toString();
+    type = json['type'].toString() == Type.image.name ? Type.image :Type.text;
+    fromId = json['fromId'].toString();
+    sent = json['sent'].toString();
+  }
+
+  Map<String, dynamic> toJson() {
+    final data = <String, dynamic>{};
+    data['toId'] = toId;
+    data['msg'] = msg;
+    data['read'] = read;
+    data['type'] = type.name;
+    data['fromId'] = fromId;
+    data['sent'] = sent;
+    return data;
+  }
+}
+
+/// enum is better than the row string comparison
+enum Type { text, image }
+/// 'image' == Type.image.name --> true
+/// 'image' == Type.image --> false
